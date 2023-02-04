6eb4dfd5ee780a1b9b99105439bff38bf3345f29
Supatra-P <78303835+Supatra-P@users.noreply.github.com>
2023-02-04 23:12:24 +0700

    uitest

--- src/components/vzeroCalculator.js
+++ src/components/vzeroCalculator.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function VzeroCalculator() {
     return (
         <div className="bg-white px-6 pt-10 pb-8 w-96 shadow-xl ring-1 ring-gray-900/5 rounded-lg">
-            <div className="bg-[#EBB3C2] flex flex-col justify-center shadow-xl h-10 m-1 rounded-lg">
+            <div className="bg-[#EBB3C2] flex items-center justify-end pr-4 shadow-xl h-10 m-1 rounded-lg">
                 <p className='text-white'>Display screen result</p>
             </div>
             <div className='sqBtn'>
